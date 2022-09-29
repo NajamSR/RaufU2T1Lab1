0331f9b03823c4940b3db4404492420898b31a6d
NajamSR <najamsaqibr@nycstudents.net>
2022-09-29 12:30:07 -0400

    initial commit

---
+++ src/CatRunner.java
@@ -0,0 +1,12 @@
+public class CatRunner {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Fluffy", 5, 8.5);
+        cat1.introduce();
+
+
+        Cat cat2 = new Cat("Puffy", 8, 9.5);
+        cat2.introduce();
+
+
+    }
+}
